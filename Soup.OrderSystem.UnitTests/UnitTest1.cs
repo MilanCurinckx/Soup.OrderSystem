4af26e9bef579145afad5a8ffbaa817ccea0fcc7
Can Ofsoup <Milancurinckx@gmail.com>
2025-05-19 13:26:59 +0200

    Created StockAction object & a unittesting project

---
+++ Soup.OrderSystem.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Soup.OrderSystem.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Soup.Ordersystem.Objects/StockAction.cs
@@ -0,0 +1,17 @@
+﻿using Soup.Ordersystem.Objects.Order;
+
+namespace Soup.Ordersystem.Objects
+{
+    public class StockAction
+    {
+        public Products Product { get; set; }
+        public StockActionEnum ActionEnum { get; set; }
+        public int Amount { get; set; }
+        public enum StockActionEnum
+        {
+            Add = 1,
+            Remove = 2,
+            Reserve = 3,
+        }
+    }
+}
