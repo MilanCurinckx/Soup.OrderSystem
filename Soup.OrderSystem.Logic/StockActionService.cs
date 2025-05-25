2b9ca2dee79178e1f6b62da87e84a1bf84ebb5bd
Can Ofsoup <Milancurinckx@gmail.com>
2025-05-25 13:30:46 +0200

    Added  GetStockActionsByProductAsync, GetCurrentStockAmountAsync and GetAvailableStockAmountAsync methods. Added comments to StockActionService. Added Null checks to StockActionService.

--- Soup.OrderSystem.Logic/StockActionService.cs
+++ Soup.OrderSystem.Logic/StockActionService.cs
@@ -8,6 +8,11 @@ namespace Soup.OrderSystem.Logic
     public class StockActionService
     {
         private OrderContext _orderContext = new();
+        /// <summary>
+        /// Creates a new stock action and saves it to the db
+        /// </summary>
+        /// <param name="stockActionDTO"></param>
+        /// <returns></returns>
         public async Task CreateStockAction(StockActionDTO stockActionDTO)
         {
             StockAction stockAction = new();
@@ -19,30 +24,122 @@ namespace Soup.OrderSystem.Logic
             _orderContext.Add(stockAction);
             await _orderContext.SaveChangesAsync();
         }
+
+        /// <summary>
+        ///  returns the stock action corresponding to the given id, if not found it will return null
+        /// </summary>
+        /// <param name="stockActionId"></param>
+        /// <returns></returns>
         public async Task<StockAction> GetStockActionAsync(int stockActionId)
         {
             var stockAction = await _orderContext.Stock_Actions.Where(s => s.Id == stockActionId).FirstOrDefaultAsync();
             return stockAction;
         }
+        /// <summary>
+        /// returns a list of all the stock actions in the db
+        /// </summary>
+        /// <returns></returns>
         public async Task<IEnumerable<StockAction>> GetStockActionsListAsync()
         {
             var stockActions = await _orderContext.Stock_Actions.ToListAsync();
             return stockActions;
         }
-        public async Task<IEnumerable<StockAction>>GetStockActionsAsyncByType(int stockAction)
+
+        /// <summary>
+        /// returns a list of stock actions depending on what value has been passed along. 1 = all the stock actions that add stock. 2 = all the stock actions that remove stock. 3 = all the stock actions that reserve stock.
+        /// </summary>
+        /// <param name="stockAction"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<StockAction>> GetStockActionsAsyncByType(int stockAction)
         {
             List<StockAction> stockActionsList = new();
             switch (stockAction)
             {
                 case (int)StockActionEnum.Add:
-                    stockActionsList = await _orderContext.Stock_Actions.Where(s=> s.StockActions == StockActionEnum.Add).ToListAsync(); 
+                    stockActionsList = await _orderContext.Stock_Actions.Where(s => s.StockActions == StockActionEnum.Add).ToListAsync();
                     break;
                 case (int)StockActionEnum.Remove:
                     stockActionsList = await _orderContext.Stock_Actions.Where(s => s.StockActions == StockActionEnum.Remove).ToListAsync();
                     break;
-                case(int)StockActionEnum.Reserve:
-                    stockActionsList
+                case (int)StockActionEnum.Reserve:
+                    stockActionsList = await _orderContext.Stock_Actions.Where(s => s.StockActions == StockActionEnum.Reserve).ToListAsync();
+                    break;
             }
+            return stockActionsList;
+        }
+        /// <summary>
+        /// returns a list of all the stock actions for a specific product
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<StockAction>> GetStockActionsByProductAsync(int productId)
+        {
+            var stockActions = await _orderContext.Stock_Actions.Where(s => s.ProductId == productId).ToListAsync();
+            return stockActions;
+        }
+        /// <summary>
+        /// Returns the current stock amount of an item. First gets a list of all the stock actions of that product, and then puts all of the add and remove stock actions in separate lists. After that the sum of the stock action amount is calculated for the added & removed lists respectively. The end result is the sum of the of the added amount minus the sum of the removed amount. 
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <returns></returns>
+        public async Task<int> GetCurrentStockAmountAsync(int productId)
+        {
+            var productStockActions = await GetStockActionsByProductAsync(productId);
+            var stockAddedList = new List<StockAction>();
+            int stockAddedTotal = 0;
+            var stockRemovedList = new List<StockAction>();
+            int stockRemovedTotal = 0;
+            int CurrentstockAmount = 0;
+            if (productStockActions == null)
+            { }
+            else
+            {
+                foreach (var stockAction in productStockActions)
+                {
+                    if (stockAction.StockActions == StockActionEnum.Add)
+                    {
+                        stockAddedList.Add(stockAction);
+                    }
+                    if (stockAction.StockActions == StockActionEnum.Remove)
+                    {
+                        stockRemovedList.Add(stockAction);
+                    }
+                }
+                stockAddedTotal = stockAddedList.Sum(s => s.Amount);
+                stockRemovedTotal = stockRemovedList.Sum(s => s.Amount);
+                CurrentstockAmount = stockAddedTotal - stockRemovedTotal;
+            }
+                return CurrentstockAmount;
+        }
+        /// <summary>
+        /// Returns the available stock amount of an item.
+        /// Gets a list of all the stock actions of a product and puts all of the reserved actions of that product in a separate list. Take the sum of all the amount in the reserved list and subtract it from the value gotten from GetCurrentStockAmount.
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <returns></returns>
+        public async Task<int> GetAvailableStockAmountAsync(int productId)
+        {
+            var productStockActions = await GetStockActionsByProductAsync (productId);
+            var currentStockAmount = await GetCurrentStockAmountAsync(productId);
+            var reservedStockList = new List<StockAction>();
+            int reservedStockAmount = 0;
+            int availableStockAmount = 0;
+            if (productStockActions == null)
+            { }
+            else
+            {
+                foreach (var stockAction in productStockActions)
+                {
+                    if (stockAction.StockActions == StockActionEnum.Reserve)
+                    {
+                        reservedStockList.Add(stockAction);
+                    }
+                }
+                reservedStockAmount = reservedStockList.Sum(s => s.Amount);
+                availableStockAmount = currentStockAmount - reservedStockAmount;
+            }
+            return availableStockAmount;
+
         }
         //public async Task UpdateStockActionAsync(StockActionDTO stockActionDTO)
         //{
